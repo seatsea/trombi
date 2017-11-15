17e7e723835e542699ff2c069c3edf074ca47197
QuentinGar <gq602735@rt202p02.iutnice.unice.fr>
2017-11-15 15:27:08 +0100

    Syntaxe pour modifier les erreurs

--- cor_html.py
+++ cor_html.py
@@ -5,3 +5,23 @@ Cette fonction doit prendre en un fichier html et corriger ces erreurs
 
 def cor_html(src):
     file = open(src)  # Ouvre le fichier
+
+
+
+
+
+
+
+
+
+# !/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+f = file("trombicorrige.html","r")    # ouvrir le fichier
+chaine = f.read()                   # le charger dans une chaine de caractères
+f.close()                           # fermer le fichier
+result=chaine.replace("</header>", "</header></body>") # remplacer les données que tu souhaites
+f = file("trombicorrige.html","w")    # ouvrir le fichier de sortie
+                       # en écriture  Tu peux ouvrir le même si tu veux l'écraser
+f.write(result)                     # écrire le résultat dans le fichier
+f.close()                           # fermer le fichier

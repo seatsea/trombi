ff896966e333081c2e923d45a460c116e147c17d
seatsea <seatsea@gmail.com>
2017-11-27 10:24:00 +0100

    Placeholders fonctions

--- trombi.py
+++ trombi.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-import cor_html
-import parse_html
+from cor_html import cor_html
+from parse_html import parse_html
 
 src_html = "index.html"  # Nom du fichier html a scanner
 src_csv = "rt2.csv"  # Nom du CSV
+
+html = cor_html(src_html) # Corrige l'html
+
+list_h = parse_html(html) # Prend l'html et retourne une liste de persones
+
+list_c = parse_csv(src_csv) # Prend le csv et retourne une liste
+
+list_m = merge_list(list_h,list_c) # Regroupe les deux listes par noms
+
+groups = scan_groups(list_m)
+
+for g in groups:
+    pass

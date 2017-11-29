1785a157277b393636aed7fef38e0699803ee78a
RBrieulle <br602047@rt202p02.iutnice.unice.fr>
2017-11-29 10:56:23 +0100

    parse_html

--- parse_csv.py
+++ parse_csv.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import csv											# Importation du module csv
 def parse_csv(file):								# Définition de la fonction
-	ouverture=open(file,'r')					# Ouverture du fichier csv en mode "read"
-	lignes=csv.reader(ouverture,delimiter=';')		# Lecture du fichier csv avec un délimiteur ';'	
-	liste_e=[]										# Définition de la liste que l'on va remplir avec le for qui suit
+	ouverture = open(file,'r')					    # Ouverture du fichier csv en mode "read"
+	lignes = csv.reader(ouverture,delimiter=';')		# Lecture du fichier csv avec un délimiteur ';'	
+	liste_e = []										# Définition de la liste que l'on va remplir avec le for qui suit
 	
 	for i in lignes :								# Définition de la boocle 
-		liste_e+=lignes
+		liste_e += lignes
 									
 	
--- parse_html.py
+++ parse_html.py
@@ -1,9 +1,27 @@
 """
-Cette fonction doit prendre en un fichier html correcte en entré
+Cette fonction doit prendre en un fichier html correcte en entrée
 Utilise html parse
-Crée un csv avec nom;prenom;lien image
+Crée une liste de liste avec nom;prenom;lien image
+
 """
+#!/usr/bin/python3
+import os
+	
 
+from bs4 import BeautifulSoup  
 
-def parse_html(src):
-    file = open(src)  # Ouvre le fichier
+def parse_html(file_h): 
+	opening = open(file_h, "r")
+	soup = BeautifulSoup(opening, "html.parser")
+	a = soup.find_all("td", valign="top", width="16%", align="center")
+	liste = []
+	personne = []
+	
+	for td in a:
+		print(td)
+		
+		
+		liste += personne
+		
+
+parse_html('M3206.html')

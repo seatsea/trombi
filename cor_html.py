c248e5a7278968ff9fe06e320c548ff155f44bf8
QuentinGar <gq602735@rt202p02.iutnice.unice.fr>
2017-11-29 09:24:46 +0100

    Fichier correction final

--- cor_html.py
+++ cor_html.py
@@ -1,27 +1,26 @@
 # !/usr/bin/python3
 #-*- coding: utf-8 -*-
-import os
 
-"""
-Cette fonction doit prendre en un fichier html et corriger ces erreurs
-"""
+import os
 
 def correction(file_o):
 	file_split = file_o.split(".")
 	file_split[-2] += "_correct"
 	file_c = ".".join(file_split)
-	print(file_c)
-	pass
-	os.system("cp %s %s"%(file_o, file_c))
-	f_c = open(file_c,"w")  # ouvrir le fichier
-	chaine = f_c.read()                   # le charger dans une chaine de caractères
-	f_c.close()                           # fermer le fichier
-	result0=chaine.replace("</header>", "</header></body>") # remplacer les données que tu souhaites
-	result1=chaine.replace("cellpading", "cellpadding") # remplacer les données que tu souhaites
-	result2=chaine.replace("nowrap=\"true\" align=\"left\"", "align=\"left\" nowrap") # remplacer les données que tu souhaites       
-	result3=chaine.replace("<td valign=\"center\"", "<td valign=\"middle\"") # remplacer les données que tu souhaites
-	f_c.write(result)                     # écrire le résultat dans le fichier
-	f_c.close()
-	return(file_c)                           # fermer le fichier
+	f_init = open(file_o,"r")  # ouvrir le fichier
+	                                    
+	contenu = ''
+	for i in f_init :
+		line = i
+		line=line.replace("</header>", "</header></body>") # remplacer les données que tu souhaites
+		line=line.replace("cellpading", "cellpadding") # remplacer les données que tu souhaites
+		line=line.replace("nowrap=\"true\" align=\"left\"", "align=\"left\" nowrap") # remplacer les données que tu souhaites       
+		line=line.replace("<td valign=\"center\"", "<td valign=\"middle\"") # remplacer les données que tu souhaites
+		contenu = contenu+line
 
-correction('trombinos.unice.fr.html')
+	
+	f_c=open(file_c,'w')
+	f_c.write(contenu)                    # écrire le résultat dans le fichier
+	f_c.close()                      # fermer le fichier
+
+correction("trombinos.unice.fr.html")

7750d1fe05bc5ac5d4d18a6382fc4e6b01ca28bb
seatsea <seatsea@gmail.com>
2017-11-29 10:17:39 +0100

    Reformat parse_csv

--- parse_csv.py
+++ parse_csv.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-import csv											# Importation du module csv
-def parse_csv(file):								# Définition de la fonction
-	ouverture=open(file, 'r', encoding='utf-8')					# Ouverture du fichier csv en mode "read"
-	lignes=csv.reader(ouverture,delimiter=';')		# Lecture du fichier csv avec un délimiteur ';'	
-	liste_e=[]										# Définition de la liste que l'on va remplir avec le for qui suit
-	
-	for i in lignes :								# Définition de la boocle 
-		liste_e+=lignes
-	
-	print(liste_e)								
-	
+import csv  # Importation du module csv
+
+
+def parse_csv(file):  # Définition de la fonction
+    ouverture = open(file, 'r', encoding='utf-8')  # Ouverture du fichier csv en mode "read"
+    lignes = csv.reader(ouverture, delimiter=';')  # Lecture du fichier csv avec un délimiteur ';'
+    liste_e = []  # Définition de la liste que l'on va remplir avec le for qui suit
+    liste_e += lignes
+
+    for i in range(0,len(liste_e)):
+        liste_e[i] = liste_e[i][0:3]
+
+    print(liste_e)
+
+
 parse_csv("rt2.csv")

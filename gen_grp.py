0afdd087799be0f302b9c58d8263eeffc3b9e8a2
Quentin Garnier <33453400+QuentinGar@users.noreply.github.com>
2017-12-05 23:14:49 +0100

    Complément

--- gen_grp.py
+++ gen_grp.py
@@ -5,8 +5,8 @@ def construct_grp(list_grp):
 
     for i in list_grp:
         file_init = open("%s.html" %(i), 'w')
-        a = file_init.write('<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>Groupe %s</title></head><body><p>Page du Groupe %s </p></body></html>' %(i,i))
-        <table width = "100%"><tbody><tr><td width = "16%" valign = "top" align = "center" ><img src = "trombinos.unice.fr_fichiers/image_043.jpeg" width = "120" height = "160&quot;" > <br> Amin Abdessadki < / td > < td width = "16%" valign = "top" align = "center" >
+        file_init.write('<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>Groupe %s</title></head><body><p>Page du Groupe %s </p></body></html>' %(i,i))
+        <table width = \"100%\"><tbody><tr><td width = \"16%\" valign = \"top\" align = \"center\" ><img src = \"trombinos.unice.fr_fichiers/image_043.jpeg\" width = \"120\" height = \"160&quot;\" > <br> Amin Abdessadki </td> <td width = \"16%\" valign = \"top\" align  \"center\" >
 
     file_init.close()
 

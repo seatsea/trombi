49b9c188d3a0b46110d6d5069791e1f77b29948b
Quentin Garnier <33453400+QuentinGar@users.noreply.github.com>
2017-12-05 23:33:11 +0100

    Complément

--- gen_grp.py
+++ gen_grp.py
@@ -5,9 +5,10 @@ def construct_grp(list_grp):
 
     for i in list_grp:
         file_init = open("%s.html" %(i), 'w')
-        file_init.write('<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>Groupe %s</title></head><body><p>Page du Groupe %s </p></body></html>' %(i,i))
-        <table width = \"100%\"><tbody><tr><td width = \"16%\" valign = \"top\" align = \"center\" ><img src = \"trombinos.unice.fr_fichiers/image_043.jpeg\" width = \"120\" height = \"160&quot;\" > <br> Amin Abdessadki </td> <td width = \"16%\" valign = \"top\" align  \"center\" >
+        file_init.write("<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>Groupe %s</title></head><body><p>Page du Groupe %s </p><table width = \"100%\">") %(i,i))
 
+        for j in list_stu:
+            file_init.write("<tbody> <tr> <td width = \"16%\" valign = \"top\" align = \"center\" ><img src = \"%s\" width = \"120\" height = \"160&quot;\" > <br> %s %s </td> <td width = \"16%\" valign = \"top\" align  \"center\" ></tr></tbody></table></body></html>") %(j[],j[],j[],j[])
     file_init.close()
 
 construct_grp(['1A','1B','2A','2B','3'])

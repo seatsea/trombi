ea6e4006603c2d809777d9ce492ffa9ad9f0bceb
seatsea <seatsea@gmail.com>
2017-12-06 16:30:35 +0100

    Fix image bug

--- gen_grp.py
+++ gen_grp.py
@@ -7,7 +7,7 @@ def construct_grp(grp,list_personne):
     file_init.write('<html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>Groupe ' + grp + '"</title></head><body><p>Page du Groupe ' + grp + ' </p><table width = "100%"><tbody> <tr> ' + "\n")
     c = 0
     for personne in list_personne:
-        file_init.write('<td width = "16%" valign = "top" align = "center" ><img src = "/' + personne[1] + '" width = "120" height = "160&quot;" > <br> ' + personne[0] + ' </td>' "\n")
+        file_init.write('<td width = "16%" valign = "top" align = "center" ><img src = "' + personne[1] + '" width = "120" height = "160&quot;" > <br> ' + personne[0] + ' </td>' "\n")
         c += 1
         if c > 3:
             file_init.write("</tr> \n <tr>")
